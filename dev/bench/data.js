686b34030e75474c3e83c6218e4463e3e38e627b
github-action-benchmark <github@users.noreply.github.com>
2023-03-11 16:47:55 +0000

    add Rust Benchmark (cargo) benchmark result for e8b81c07258010916dc62c9db23199596a973321

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678552816802,
+  "lastUpdate": 1678553275389,
   "repoUrl": "https://github.com/eleijonmarck/rustybucket",
   "entries": {
     "Rust Benchmark": [
@@ -23,6 +23,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/eleijonmarck/rustybucket/commit/d7583e4787c0ee6577388a778d8645b25cb60a1b"
         },
         "date": 1678552816181,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::slice_a_series",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eric.leijonmarck@gmail.com",
+            "name": "Eric Leijonmarck",
+            "username": "eleijonmarck"
+          },
+          "committer": {
+            "email": "eric.leijonmarck@gmail.com",
+            "name": "Eric Leijonmarck",
+            "username": "eleijonmarck"
+          },
+          "distinct": true,
+          "id": "e8b81c07258010916dc62c9db23199596a973321",
+          "message": "benchmark",
+          "timestamp": "2023-03-11T16:47:28Z",
+          "tree_id": "0577bafab86bc61bb829ccf5245c481b10aa763c",
+          "url": "https://github.com/eleijonmarck/rustybucket/commit/e8b81c07258010916dc62c9db23199596a973321"
+        },
+        "date": 1678553274274,
         "tool": "cargo",
         "benches": [
           {
